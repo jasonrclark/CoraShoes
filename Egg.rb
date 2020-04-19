a315a3f4d54d1ca7eedad0fa5bfad73d1c33ee80
Jason R. Clark <jason@jasonrclark.net>
2020-04-19 16:32:59 +0200

    Draw rectangles too

--- Egg.rb
+++ Egg.rb
@@ -1,7 +1,20 @@
 Shoes.app do
- o = oval 100,50,300,425,fill:oldlace
+  tool = "oval"
+  button "oval" do
+    tool = "oval"
+  end
+  button "rect" do
+    tool = "rect"
+  end
 
- click do |w,x,y|
-    oval x,y,25,25,fill:violet
- end
+  o = oval 100,50,300,425,fill:oldlace
+  o.pass_coordinates = true
+
+  o.click do |w,x,y|
+    if tool == "oval"
+      oval x,y,25,25,fill:lime
+    elsif tool == "rect"
+      rect x,y,25,25,fill:magenta
+    end
+  end
 end
